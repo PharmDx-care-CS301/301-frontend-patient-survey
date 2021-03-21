11bec6229b397ca12aa184e279bc857ad07fda9f
Jake Wolf <karate.wolf07@gmail.com>
2021-03-21 19:15:44 -0400

    Basic survey view created with surveyjs

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# 301-frontend-patient-survey
+Web page for patients to do their surveys
---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import * as Survey from "survey-react";
+import ReactDOM from "react-dom";
+import './App.css';
+import "survey-react/survey.css"
+
+function App() {
+  let json = {
+    "title": "PharmDx Patient survey",
+    "logoPosition": "top",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "checkbox",
+        "name": "Symptoms",
+        "title": "Please select any symptoms or side effects you are experiencing",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Dizziness"
+         },
+         {
+          "value": "item2",
+          "text": "Drowsiness"
+         },
+         {
+          "value": "item3",
+          "text": "Fatigue"
+         },
+         {
+          "value": "item4",
+          "text": "Nausea and vomiting"
+         },
+         {
+          "value": "item5",
+          "text": "Rash"
+         }
+        ]
+       },
+       {
+        "type": "text",
+        "name": "Added Symptoms",
+        "title": "Are you experiencing any other side effects or symptoms not listed above? If so, please describe them below"
+       },
+       {
+        "type": "datepicker",
+        "name": "Schedule Date",
+        "title": "If you have any questions or concerns and would like to manually schedule a follow up appointment: "
+       },
+       {
+        "type": "dropdown",
+        "name": "Schedule Time",
+        "title": "What time of day would you like to schedule your appointment for?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Morning(8am-12pm)"
+         },
+         {
+          "value": "item2",
+          "text": "Afternoon(1pm-5pm)"
+         },
+         {
+          "value": "item3",
+          "text": "Evening(6pm-8pm)"
+         }
+        ]
+       },
+       {
+        "type": "text",
+        "name": "Added comments",
+        "title": "Do you have any questions or concerns for your pharmasist or feedback to give about your experience? If so, please describe them below"
+       }
+      ]
+     }
+    ]
+   };
+   var surveyRender = <Survey.Survey json={json} />;
+   return (
+    <div className="App">
+      {surveyRender}
+    </div>
+   )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+export default App;
+
